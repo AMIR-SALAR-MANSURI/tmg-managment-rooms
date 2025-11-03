2748b96ecc9a3eb78aa3ca6e171a65087b3f3dda
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2025-11-03 12:46:55 +0330

    fixed

--- src/layout/dashboard/page-wrapper.tsx
+++ src/layout/dashboard/page-wrapper.tsx
@@ -83,10 +83,10 @@ export default function PageWrapper({
             {headChildren}
           </div>
         </div>
+        <Separator />
       </header>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         {/* <Separator /> */}
-
         <Card className="flex flex-col h-full">
           {(title || description || action) && (
             <>
