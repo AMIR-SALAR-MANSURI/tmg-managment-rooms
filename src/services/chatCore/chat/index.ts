07f552620f388a5da977aaf8a98a5d958e082088
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2025-11-03 12:48:05 +0330

    delete the chatCore services

--- src/services/chatCore/chat/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./chat.hook";
-export * from "./chat.service";
-export type {
-  EditChatRequest,
-  DeleteChatResponse,
-  SendChatRequest,
-  GetAllChatResponse,
-} from "./chat.interface";
--- src/services/chatCore/room/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./room.hook";
-export * from "./room.service";
-export type {
-  EditRoomRequest,
-  DeleteRoomResponse,
-  AddRoomRequest,
-  GetAllRoomResponse,
-} from "./room.interface";
