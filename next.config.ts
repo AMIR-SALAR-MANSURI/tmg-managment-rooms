1699781b28af992241d101375966b8f2d4beac92
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2025-11-04 14:47:12 +0330

    add redirect to next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: process.env.NEXT_PUBLIC_BASE_PATH as string,
+        basePath: false,
+        statusCode: 303,
+        permanent: undefined,
+      },
+      {
+        source: "/login",
+        destination: (process.env.NEXT_PUBLIC_BASE_PATH + "/login") as string,
+        basePath: false,
+        statusCode: 303,
+        permanent: undefined,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
